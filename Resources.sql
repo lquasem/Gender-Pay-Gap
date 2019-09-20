74b098e5ad65c2ba501a931cbb998a4df095f8cd
Loba Quasem <49497366+lquasem@users.noreply.github.com>
2019-09-20 14:57:05 -0700

    pay gap by race

---
+++ Resources.sql
@@ -0,0 +1,10 @@
+-- Create new table
+CREATE TABLE by_race (
+	Household_data TEXT,
+	Total_Workers_2017 TEXT,
+	Total_Workers_2018 TEXT,
+	Median_weekly_earnings_2017  TEXT,
+	Median_weekly_earnings_2018 TEXT
+	
+);
+Select * from by_race;
